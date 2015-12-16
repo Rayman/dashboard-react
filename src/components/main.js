93c330964fafb2115c8f27a0142410e6357dca90
Ramon Wijnands <rayman747@hotmail.com>
2015-12-16 22:20:44 +0100

    add material-ui example

---
+++ src/components/main.js
@@ -0,0 +1,78 @@
+/** In this file, we create a React component which incorporates components provided by material-ui */
+
+import React from 'react';
+import RaisedButton from 'material-ui/lib/raised-button';
+import Dialog from 'material-ui/lib/dialog';
+import ThemeManager from 'material-ui/lib/styles/theme-manager';
+import LightRawTheme from 'material-ui/lib/styles/raw-themes/light-raw-theme';
+import Colors from 'material-ui/lib/styles/colors';
+
+const containerStyle = {
+  textAlign: 'center',
+  paddingTop: 200,
+};
+
+const standardActions = [
+  {
+    text: 'Okay',
+  },
+];
+
+const Main = React.createClass({
+
+  childContextTypes: {
+    muiTheme: React.PropTypes.object,
+  },
+
+  getInitialState() {
+    return {
+      muiTheme: ThemeManager.getMuiTheme(LightRawTheme),
+    };
+  },
+
+  getChildContext() {
+    return {
+      muiTheme: this.state.muiTheme,
+    };
+  },
+
+  componentWillMount() {
+    let newMuiTheme = ThemeManager.modifyRawThemePalette(this.state.muiTheme, {
+      accent1Color: Colors.deepOrange500,
+    });
+
+    this.setState({muiTheme: newMuiTheme});
+  },
+
+  _handleRequestClose() {
+    this.setState({
+      open: false,
+    });
+  },
+
+  _handleTouchTap() {
+    this.setState({
+      open: true,
+    });
+  },
+
+  render() {
+    return (
+      <div style={containerStyle}>
+        <Dialog
+          open={this.state.open}
+          title="Super Secret Password"
+          actions={standardActions}
+          onRequestClose={this._handleRequestClose}
+        >
+          1-2-3-4-5
+        </Dialog>
+        <h1>material-ui</h1>
+        <h2>example project</h2>
+        <RaisedButton label="Super Secret Password" primary={true} onTouchTap={this._handleTouchTap} />
+      </div>
+    );
+  },
+});
+
+export default Main;
